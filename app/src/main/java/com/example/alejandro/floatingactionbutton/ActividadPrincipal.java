84355d4a070167a1219e03f02908d49660a85692
marialexculturaapp <maria.ibarra@cbtis72.edu.mx>
2019-05-12 18:24:53 -0500

    Transformar floating action button en toolbar

--- app/src/main/java/com/example/alejandro/floatingactionbutton/ActividadPrincipal.java
+++ app/src/main/java/com/example/alejandro/floatingactionbutton/ActividadPrincipal.java
@@ -94,7 +94,7 @@ public class ActividadPrincipal extends AppCompatActivity {
     }
 }
 */
-import android.os.Bundle;
+/*import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -133,4 +133,51 @@ public class ActividadPrincipal extends AppCompatActivity {
         });
     }
 
+}
+*/
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
+
+public class ActividadPrincipal extends AppCompatActivity implements View.OnClickListener {
+
+    private FABToolbarLayout morph;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        morph = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
+
+        View uno, dos, tres, cuatro;
+
+        uno = findViewById(R.id.uno);
+        dos = findViewById(R.id.dos);
+        cuatro = findViewById(R.id.cuatro);
+        tres = findViewById(R.id.tres);
+
+        fab.setOnClickListener(this);
+        uno.setOnClickListener(this);
+        dos.setOnClickListener(this);
+        tres.setOnClickListener(this);
+        cuatro.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.fab) {
+            morph.show();
+        }
+
+        morph.hide();
+    }
 }
