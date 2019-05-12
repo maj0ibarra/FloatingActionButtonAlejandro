3d5b4e22514f381fef79b559b79d11840fa2c46e
marialexculturaapp <maria.ibarra@cbtis72.edu.mx>
2019-05-12 18:02:29 -0500

    Rotar floating action button al clickearlo

--- app/src/main/java/com/example/alejandro/floatingactionbutton/ActividadPrincipal.java
+++ app/src/main/java/com/example/alejandro/floatingactionbutton/ActividadPrincipal.java
@@ -25,7 +25,7 @@ public class ActividadPrincipal extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
-import android.animation.Animator;
+/*import android.animation.Animator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -92,4 +92,45 @@ public class ActividadPrincipal extends AppCompatActivity {
             }
         });
     }
+}
+*/
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+
+public class ActividadPrincipal extends AppCompatActivity {
+
+    boolean click = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageResource(R.drawable.agregar);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click = !click;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    Interpolator interpolador = AnimationUtils.loadInterpolator(getBaseContext(),
+                            android.R.interpolator.fast_out_slow_in);
+
+                    view.animate()
+                            .rotation(click ? 45f : 0)
+                            .setInterpolator(interpolador)
+                            .start();
+                }
+            }
+        });
+    }
+
 }
